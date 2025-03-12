d79d1b4898497eb37cd84d780194de8e831faa56
Hermi-git <andargiehermela1@gmail.com>
2025-03-12 04:41:47 +0300

    How it works page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import Home from "./Pages/Home/Home";
+import HowItWorks from "./Pages/HowItWorks/HowItWorks";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 import {Routes, Route} from "react-router-dom"
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/howitworks" element={<HowItWorks/>}/>
       </Routes>
  </div>
 )
